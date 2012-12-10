06902ba2583b173e2c9975f0dfeb37144fa448c0
Eivind Hagen <eivind@spaceport.io>
2012-12-09 17:02:14 -0800

    Added shell script to verify the combined file size of the scanner directory.

--- test_scripts/personal.sh
+++ test_scripts/personal.sh
@@ -6,3 +6,6 @@ scan_path=~/Personal/Chameleon
 scan_name=Chameleon
 ruby dirscan.rb sq $scan_path tmp/$scan_name.dirscan
 ruby dirscan.rb u tmp/$scan_name.dirscan tmp/$scan_name.json
+
+echo "Calculating content_size using find + awk"
+find scan_path -type file -ls | awk '{total += $7} END {print total}'
