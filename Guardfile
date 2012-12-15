6b15e8a4103f145f846b76b90c9703ade0639091
Eivind Hagen <eivind@spaceport.io>
2012-12-14 20:44:57 -0800

    Added test to ensure ruby source files use spaces and not tabs.

--- Guardfile
+++ Guardfile
@@ -8,5 +8,6 @@ end
 guard :test do
 	watch(%r{^lib/(.+)\.rb$})	{ |m| "test/#{m[1]}_test.rb" }  
 	watch(%r{^test/(.+)\.rb$})
+	watch(%r{(.+)\.rb$}) {"test/ruby_sources_test.rb"}
 	watch("test_data/one_file/file1.txt") {"test/pathinfo.rb"}
 end
---
+++ test/ruby_sources_test.rb
@@ -0,0 +1,21 @@
+# unit_tests.rb
+#
+# http://en.wikibooks.org/wiki/Ruby_Programming/Unit_testing
+#
+require "test/unit"
+require 'redgreen'
+
+#include the classes we are testing
+require File.expand_path('../lib/dirscanner.rb', File.dirname(__FILE__))
+
+class TestRubySources < Test::Unit::TestCase
+
+  def test_use_spaces_not_tabs
+    Dir.glob("**/*.rb").each do |path|
+      text = File.read(path)
+      match = /\t/.match(text)
+      assert_nil(match, "Found tab-char in '#{path}'")
+    end
+  end
+     
+end
